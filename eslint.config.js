6cd58ac644a9ce2e5ba250c178cd4c4eee20369a
Nakaya <eniehack@outlook.jp>
2026-02-16 18:29:12 +0900

    chore(eslint): 順序の整理、足りないパッケージの追加

--- eslint.config.js
+++ eslint.config.js
@@ -11,8 +11,6 @@ export default ts.config(
 	js.configs.recommended,
 	...ts.configs.recommended,
 	...svelte.configs['flat/recommended'],
-	prettier,
-	...svelte.configs['flat/prettier'],
 	{
 		languageOptions: {
 			globals: {
@@ -23,11 +21,12 @@ export default ts.config(
 	},
 	{
 		files: ['**/*.svelte'],
-
 		languageOptions: {
 			parserOptions: {
 				parser: ts.parser
 			}
 		}
-	}
+	},
+	prettier,
+	...svelte.configs['flat/prettier']
 );
